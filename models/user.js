5c619d0521fdf7d6c3df13f23de0c5d39a1cee27
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-01-31 18:16:13 -0500

    Added models for user and projects

--- models/user.js
+++ models/user.js
@@ -1,36 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
 
+// Exporting a function that defines the User model
 module.exports = (sequelize) => {
+  // Defining the User class which extends Sequelize's Model class
   class User extends Model {}
 
+  // Initializing the User model with its schema definition
   User.init({
+    // Defining the 'id' column as an integer, primary key, auto-incrementing
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
+    // Defining the 'username' column as a unique string, cannot be null
     username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
+    // Defining the 'email' column as a unique string, cannot be null, must be a valid email format
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true,
+        isEmail: true, // Validator to ensure the email is in valid format
       },
     },
+    // Defining the 'passwordHash' column to store hashed passwords, cannot be null
     passwordHash: {
       type: DataTypes.STRING,
       allowNull: false
     },
   }, {
-    sequelize,
-    modelName: 'User'
+    sequelize, // Associating this model with the Sequelize instance
+    modelName: 'User' // Setting the name of the model
   });
 
+  // Returning the User model
   return User;
 };
