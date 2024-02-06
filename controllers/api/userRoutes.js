f36323002f5260d0d7c0980048ec4760165e5910
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-02-05 20:35:04 -0500

    Fixed search on /login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,21 +18,18 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+  
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
@@ -48,7 +45,6 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
